9a58114814eb38f0e1f24f1b9a8731f11249aaff
Zakaria moulayrchid <zakmoulay07@gmail.com>
2023-04-13 00:22:53 -0400

    Third exercise 2D even print

---
+++ src/TwoDarrayEvenOdd.java
@@ -0,0 +1,19 @@
+import javax.imageio.metadata.IIOMetadataFormatImpl;
+
+public class TwoDarrayEvenOdd {
+    public static void main(String[] args) {
+        int[][] array = {{1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}
+
+        };
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] % 2 == 0)
+                    System.out.print(array[i][j] + " ");
+            }
+        }
+        System.out.println();
+
+    }
+}
