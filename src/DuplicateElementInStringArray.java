85dfbacc3ee92bc08f3b2869d2842e04037c1c8f
Zakaria moulayrchid <zakmoulay07@gmail.com>
2023-04-14 00:02:12 -0400

    changes done

--- src/DuplicateElementInStringArray.java
+++ src/DuplicateElementInStringArray.java
@@ -3,13 +3,13 @@ import java.util.Set;
 
 public class DuplicateElementInStringArray {
     public static void main(String[] args) {
-        String[] stArray = {"Zak","Ola","James","peter","Sabrina","Zak","Sabrina"};
+        String[] stArray = {"Zak", "Ola", "James", "peter", "Sabrina", "Zak", "Sabrina"};
         Set<String> mySet = new HashSet<>();
-        Set<String> duplicateSet = new HashSet<>();
+        Set<String> ansSet = new HashSet<>();
         for (int i = 0; i < stArray.length; i++) {
             if ( !mySet.add(stArray[i]))
-                duplicateSet.add(stArray[i]);
+                ansSet.add(stArray[i]);
         }
-        System.out.println(duplicateSet);
+        System.out.println(ansSet);
     }
 }
