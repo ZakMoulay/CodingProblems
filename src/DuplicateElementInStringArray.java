3586d6fb66bd75701375dbec283f9ffdfb1099fd
Zakaria moulayrchid <zakmoulay07@gmail.com>
2023-04-20 15:32:13 -0400

    some revisions

--- src/DuplicateElementInStringArray.java
+++ src/DuplicateElementInStringArray.java
@@ -3,7 +3,7 @@ import java.util.Set;
 
 public class DuplicateElementInStringArray {
     public static void main(String[] args) {
-        String[] stArray = {"Zak", "Ola", "James", "peter", "Sabrina", "Zak", "Sabrina"};
+        String[] stArray = {"Zak", "sabrina","zak","Ola", "James", "peter", "Sabrina", "Zak", "Sabrina"};
         Set<String> mySet = new HashSet<>();
         Set<String> ansSet = new HashSet<>();
         for (int i = 0; i < stArray.length; i++) {
@@ -11,5 +11,15 @@ public class DuplicateElementInStringArray {
                 ansSet.add(stArray[i]);
         }
         System.out.println(ansSet);
+    }
+    public static void printDuplicate(String[] st){
+        for (int i = 0; i < st.length; i++) {
+            for (int j = i+1; j < st.length ; j++) {
+                if ( st[i].equals(st[j]))
+                    System.out.println(st[i]+ " ");
+            }
+        }
+
+
     }
 }
--- src/MaxMinInArray.java
+++ src/MaxMinInArray.java
@@ -2,15 +2,16 @@ import java.util.Arrays;
 
 public class MaxMinInArray {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,5,6,9};
-//        int maxValue = arr[0];
-//        int minValue = arr[0];
-//        for (int i = 0; i < arr.length ; i++) {
-//            if ( arr[i] > maxValue) maxValue = arr[i];
-//            if ( arr[i] < minValue) minValue = arr[i];
-//
-        Arrays.sort(arr);
-        System.out.println("The min number is " + arr[0]);
-        System.out.println("The max number is " + arr[arr.length-1]);
+        int[] arr = {1,2,-3,5,44,9};
+        int maxValue = Integer.MIN_VALUE;
+        int minValue = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length ; i++) {
+            if ( arr[i] > maxValue) maxValue = arr[i];
+            if ( arr[i] < minValue) minValue = arr[i];
+        }
+
+        //Arrays.sort(arr);
+        System.out.println("The min number is " + minValue);
+        System.out.println("The max number is " + maxValue);
     }
 }
