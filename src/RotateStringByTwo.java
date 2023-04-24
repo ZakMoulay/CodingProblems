343a475f994a6ee7e3b30973b2664f5fee93cabd
Zakaria moulayrchid <zakmoulay07@gmail.com>
2023-04-23 21:13:42 -0400

    nTimes

--- src/RotateStringByTwo.java
+++ src/RotateStringByTwo.java
@@ -4,6 +4,7 @@ public class RotateStringByTwo {
         // rotate string by 2 places clockwise
         // s1 = "abcde"  s2 = "cdeab"
         System.out.println(rotateByTwo("amazon"));
+        System.out.println(rotateByNTimes("amazon", 2));
     }
     public static String rotateByTwo(String st){
         if ( st.isEmpty()) return "";
@@ -14,6 +15,18 @@ public class RotateStringByTwo {
         }
         sb.append(st.charAt(0));
         sb.append(st.charAt(1));
+        return sb.toString();
+    }
+    public static String rotateByNTimes(String st,int n){
+        if ( st.isEmpty()) return "";
+        char[] arr = st.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = n; i < st.length() ; i++) {
+            sb.append(arr[i]);
+        }
+        for (int i = 0; i < n ; i++) {
+            sb.append(st.charAt(i));
+        }
         return sb.toString();
     }
 
